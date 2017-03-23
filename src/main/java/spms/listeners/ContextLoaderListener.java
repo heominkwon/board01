f7bd1d65784375a988c8ec9002d98951e6add89b
023018 <023018@IC0-W-N02972.ssg.global>
2017-03-23 14:04:19 +0900

    20170323나머지

---
+++ src/main/java/spms/listeners/ContextLoaderListener.java
@@ -0,0 +1,30 @@
+package spms.listeners;
+
+// SqlSessionFactory 객체 준비
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+  static ClassPathXmlApplicationContext applicationContext;
+  
+  public static ClassPathXmlApplicationContext getApplicationContext() {
+    return applicationContext;
+  }
+   
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    try {
+      applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+    } catch(Throwable e) {
+      e.printStackTrace();
+    }
+  }
+  
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {}
+}
